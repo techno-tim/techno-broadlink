94795a6d1f6d627088fb5f4544e9d52d248f381e
Timothy Stewart <timothystewart6@gmail.com>
2021-10-06 23:01:19 -0500

    fix(theme): fixed deprecation warning

--- frontend/src/config/theme.js
+++ frontend/src/config/theme.js
@@ -1,6 +1,6 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createTheme } from '@material-ui/core/styles';
 
-const theme = createMuiTheme({
+const theme = createTheme({
   palette: {
     primary: {
       main: '#051a1f',
